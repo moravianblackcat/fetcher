b4ca4eae3ab48c2078ee21be2ee527c49148fc83
Daniel Polách <polachdan@gmail.com>
2025-06-22 08:37:58 +0200

    FetcherApplicationTests context loading fix

--- src/test/java/cz/dan/fetcher/FetcherApplicationTests.java
+++ src/test/java/cz/dan/fetcher/FetcherApplicationTests.java
@@ -3,7 +3,14 @@ package cz.dan.fetcher;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.datasource.url=jdbc:h2:mem:testdb",
+		"spring.datasource.driver-class-name=org.h2.Driver",
+		"spring.datasource.username=sa",
+		"spring.datasource.password=",
+		"spring.jpa.hibernate.ddl-auto=none",
+		"spring.liquibase.enabled=false"
+})
 class FetcherApplicationTests {
 
 	@Test
