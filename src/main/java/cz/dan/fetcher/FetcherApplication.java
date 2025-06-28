8da02e0a2d513efcd1c4ef39c5174087660f4625
Daniel Polách <polachdan@gmail.com>
2025-06-28 16:56:43 +0200

    EnableEncryptableProperties

--- src/main/java/cz/dan/fetcher/FetcherApplication.java
+++ src/main/java/cz/dan/fetcher/FetcherApplication.java
@@ -1,5 +1,6 @@
 package cz.dan.fetcher;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
@@ -7,6 +8,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableEncryptableProperties
 @EnableFeignClients
 public class FetcherApplication {
 
