8f7801b0454fc59234075c1d0a34e58cf0afa7f5
Daniel Polách <polachdan@gmail.com>
2025-07-02 20:12:19 +0200

    sportmonks.api.token fix

--- src/main/java/cz/dan/fetcher/domain/football/request/entity/RequestRepository.java
+++ src/main/java/cz/dan/fetcher/domain/football/request/entity/RequestRepository.java
@@ -1,9 +1,11 @@
 package cz.dan.fetcher.domain.football.request.entity;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 
 import java.util.List;
 
+@NoRepositoryBean
 public interface RequestRepository<T> extends CrudRepository<T, RequestId> {
 
     List<T> findOldestForProcessing(int limit);
